a716b625310fa11e9ed8dffca3ca283fcfd722a5
malcolmBurdorf <irishsurfer42@gmail.com>
2024-04-17 19:44:19 +0200

    aligned tables and cleaned up files and redirected download buttons to the mirrors

--- navigation.js
+++ navigation.js
@@ -2,7 +2,6 @@ document.write("\
 <nav class=\"navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top\">\
   <div class=\"container\">\
     <a class=\"navbar-brand col-lg-2\" href=\"overview.html\">\
-        <!--img src=\"images/logo.png\" width=100% alt=\"Megvii Technology\"-->\
     </a>\
     <button class=\"navbar-toggler navbar-toggler-right\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarResponsive\" aria-controls=\"navbarResponsive\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\
       <span class=\"navbar-toggler-icon\"></span>\
